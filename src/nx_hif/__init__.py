2bf549c0baa11e5cd3ca65d094d1993095934648
Martin Coll <mcoll@dc.uba.ar>
2024-10-22 10:53:40 -0300

    fixes v0.0.2

--- src/nx_hif/__init__.py
+++ src/nx_hif/__init__.py
@@ -4,27 +4,31 @@ import json
 def write_hif(G, path):
     pass
 
-def add_incidence(G: nx.DiGraph, incidence):
+def add_incidence(G: nx.Graph, incidence):
     edge_id = incidence["edge"]
     node_id = incidence["node"]
     G.add_node(edge_id, bipartite=1)
     G.add_node(node_id, bipartite=0)
     G.add_edge(edge_id, node_id)
 
-def add_edge(G: nx.DiGraph, edge):
+def add_edge(G: nx.Graph, edge):
     attrs = edge.get("attr", {})
     edge_id = edge["edge"]
     if "weigth" in edge:
         attrs["weigth"] = edge["weigth"]
     for attr_key, attr_value in attrs.items():
+        if not G.has_node(edge_id):
+            G.add_node(edge_id, bipartite=1)
         G.nodes[edge_id][attr_key] = attr_value
 
-def add_node(G: nx.DiGraph, node):
+def add_node(G: nx.Graph, node):
     attrs = node.get("attr", {})
     node_id = node["node"]
     if "weigth" in node:
         attrs["weigth"] = node["weigth"]
     for attr_key, attr_value in attrs.items():
+        if not G.has_node(node_id):
+            G.add_node(node_id, bipartite=0)
         G.nodes[node_id][attr_key] = attr_value
 
 def read_hif(path):
@@ -36,7 +40,7 @@ def read_hif_data(data):
     G_attrs = data.get("metadata", {})
     if "network-type" in data:
         G_attrs["network-type"] = data["network-type"]
-    G = nx.DiGraph(**G_attrs)
+    G = nx.Graph(**G_attrs)
     for i in data["incidences"]:
         add_incidence(G, i)
     for e in data["edges"]:
