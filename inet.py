df02540273ba005e220b19eaf4a94b7fff5939b1
Martin Coll <mcoll@dc.uba.ar>
2025-07-24 17:27:12 +0000

    add basic functions

--- inet.py
+++ inet.py
@@ -20,9 +20,73 @@ def annihilate_erase_erase(inet: nx.Graph):
                     # erase-erase annihilation
                     if inet.nodes[c1]["tag"] == "erase" and \
                         inet.nodes[c2]["tag"] == "erase":
+                        erase += [u, c1, c2]
+    inet.remove_nodes_from(erase)
+
+def annihilate_construct_duplicate(inet: nx.Graph):
+    # find all active wires
+    erase = []
+    for u, d in inet.nodes(data=True):
+        # u is a wire
+        if d["bipartite"] == 0:
+            # u connects two combinators
+            if len(inet[u]) == 2:
+                c1, c2 = inet[u]
+                # u connects two principal ports
+                if inet[u][c1]["index"] == 0 and \
+                    inet[u][c2]["index"] == 0:
+                    # erase-erase annihilation
+                    if inet.nodes[c1]["tag"] == "erase" and \
+                        inet.nodes[c2]["tag"] == "erase":
                         erase.append(u)
     for u in erase:
         c1, c2 = inet[u]
         inet.remove_node(u)
         inet.remove_node(c1)
         inet.remove_node(c2)
+
+def inet_add_erase(inet: nx.Graph):
+    n = inet.number_of_nodes()
+    inet.add_node(n, bipartite=1, tag="erase")
+    inet.add_node(n+1, bipartite=0)
+    inet.add_edge(n, n+1, index=0)
+    return n
+
+def inet_add_construct(inet: nx.Graph):
+    n = inet.number_of_nodes()
+    inet.add_node(n, bipartite=1, tag="construct")
+    inet.add_node(n+1, bipartite=0)
+    inet.add_node(n+2, bipartite=0)
+    inet.add_node(n+3, bipartite=0)
+    inet.add_edge(n, n+1, index=0)
+    inet.add_edge(n, n+2, index=1)
+    inet.add_edge(n, n+3, index=2)
+    return n
+
+def inet_add_duplicate(inet: nx.Graph):
+    n = inet.number_of_nodes()
+    inet.add_node(n, bipartite=1, tag="duplicate")
+    inet.add_node(n+1, bipartite=0)
+    inet.add_node(n+2, bipartite=0)
+    inet.add_node(n+3, bipartite=0)
+    inet.add_edge(n, n+1, index=0)
+    inet.add_edge(n, n+2, index=1)
+    inet.add_edge(n, n+3, index=2)
+    return n
+
+def inet_connect_ports(inet: nx.Graph, p0, p1):
+    u0, i0 = p0
+    u1, i1 = p1
+    old_ports = []
+    for _, b, i in inet.edges(u0, data="index"):
+        if i == i0:
+            old_ports.append(b)
+    for _, b, i in inet.edges(u1, data="index"):
+        if i == i1:
+            old_ports.append(b)
+    inet.remove_nodes_from(old_ports)
+    n = inet.number_of_nodes()
+    inet.add_node(n, bipartite=0)
+    inet.add_edge(n, u0, index=i0)
+    inet.add_edge(n, u1, index=i1)
+    return n
--- test_inet.py
+++ test_inet.py
@@ -1,14 +1,27 @@
 import networkx as nx
-from inet import annihilate_erase_erase
+from inet import *
 
 
 def test_annihilate_erase_erase():
     inet = nx.Graph()
+    u = inet_add_erase(inet)
+    v = inet_add_erase(inet)
+    inet_connect_ports(inet, (u, 0), (v, 0))
+    annihilate_erase_erase(inet)
+    assert len(inet) == 0
+
+
+def test_annihilate_construct_duplicate():
+    inet = nx.Graph()
+    u = inet_add_construct(inet)
+    v = inet_add_duplicate(inet)
+    inet_connect_ports(inet, (u, 0), (v, 0))
     inet.add_node(1, bipartite=0)
-    inet.add_node(2, bipartite=1, tag="erase")
-    inet.add_node(3, bipartite=1, tag="erase")
+    inet.add_node(2, bipartite=1, tag="construct")
+    inet.add_node(3, bipartite=1, tag="duplicate")
     inet.add_edge(1, 2, index=0)
     inet.add_edge(1, 3, index=0)
 
-    annihilate_erase_erase(inet)
-    assert len(inet) == 0
+    annihilate_construct_duplicate(inet)
+    assert len(inet.nodes) == 2
+    assert len(inet.edges) == 0
