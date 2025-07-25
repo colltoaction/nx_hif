57ba0b810fcdf4584d1a878e75d396e90a1784bf
Martin Coll <mcoll@dc.uba.ar>
2025-07-25 19:00:35 +0000

    annihilate_erase_erase

--- inet.py
+++ inet.py
@@ -1,11 +1,13 @@
 import networkx as nx
 
 # there is a node for each combinator
+# there is a node for each wire, including disconnected wires
 # there is an edge for each pair of connected ports
-# each wire is connected to zero, one or two combinators
+# each wire is connected to one or two combinators
 # each combinator is connected to one wire per port
+# a loop or self-connection uses multiedges for each port
 
-def find_all_active_wires(inet: nx.Graph):
+def find_all_active_wires(inet: nx.MultiGraph):
     # find all active wires
     active = []
     for u, d in inet.nodes(data=True):
@@ -14,96 +16,102 @@ def find_all_active_wires(inet: nx.Graph):
             # u connects two combinators
             if len(inet[u]) == 2:
                 c1, c2 = inet[u]
+                print(inet[u])
+                print(c1, c2)
                 # u connects two principal ports
-                if inet[u][c1]["index"] == 0 and \
-                    inet[u][c2]["index"] == 0:
-                    active.append(u)
+                if c1 != c2 and \
+                    0 in inet[u][c1] and \
+                    0 in inet[u][c2]:
+                    active.append((u, c1, c2))
     return active
 
-def annihilate_erase_erase(inet: nx.Graph):
+def annihilate_erase_erase(inet: nx.MultiGraph):
     active = []
-    for u in find_all_active_wires(inet):
-        c1, c2 = inet[u]
+    for u, c1, c2 in find_all_active_wires(inet):
+        print(u, c1, c2)
         # erase-erase annihilation
         if inet.nodes[c1]["tag"] == "erase" and \
             inet.nodes[c2]["tag"] == "erase":
-            active += [u, c1, c2]
-    inet.remove_nodes_from(active)
+            active.append([u, c1, 0])
+            active.append([u, c2, 0])
+    # TODO we never remove nodes to ensure increasing IDs
+    print(active)
+    inet.remove_edges_from(active)
 
-def commute_construct_duplicate(inet: nx.Graph):
-    # find all active wires
-    erase = []
-    for u, d in inet.nodes(data=True):
-        # u is a wire
-        if d["bipartite"] == 0:
-            # u connects two combinators
-            if len(inet[u]) == 2:
-                c1, c2 = inet[u]
-                # u connects two principal ports
-                if inet[u][c1]["index"] == 0 and \
-                    inet[u][c2]["index"] == 0:
-                    tags = {"construct", "duplicate"}
-                    found_tags = {inet.nodes[c1]["tag"], inet.nodes[c2]["tag"]}
-                    if tags == found_tags:
-                        erase += [u, c1, c2]
-    inet.remove_nodes_from(erase)
-    c0 = inet_add_construct(inet)
-    c1 = inet_add_construct(inet)
-    d0 = inet_add_duplicate(inet)
-    d1 = inet_add_duplicate(inet)
-    # rewire 2x2 secondary ports to 4 principals
-    inet_connect_ports(inet, (c0, 0), ())
-    inet_connect_ports(inet, (c1, 0), ())
-    inet_connect_ports(inet, (d0, 0), ())
-    inet_connect_ports(inet, (d1, 0), ())
-    # wire secondary ports
-    inet_connect_ports(inet, (c0, 1), (d0, 2))
-    inet_connect_ports(inet, (c0, 2), (d1, 2))
-    inet_connect_ports(inet, (c1, 1), (d0, 1))
-    inet_connect_ports(inet, (c1, 2), (d1, 1))
+def commute_construct_duplicate(inet: nx.MultiGraph):
+    active = []
+    for u, c, d in find_all_active_wires(inet):
+        tags = {"construct", "duplicate"}
+        found_tags = {inet.nodes[c]["tag"], inet.nodes[d]["tag"]}
+        if tags == found_tags:
+            active.append((u, c, d))
 
-def inet_add_erase(inet: nx.Graph):
+    for u, c, d in active:
+        c0 = inet_add_construct(inet)
+        c1 = inet_add_construct(inet)
+        d0 = inet_add_duplicate(inet)
+        d1 = inet_add_duplicate(inet)
+        # rewire 2x2 secondary ports to 4 principals
+        inet_connect_ports(inet, (c0, 0), (d, 2))
+        inet_connect_ports(inet, (c1, 0), (d, 1))
+        inet_connect_ports(inet, (d0, 0), (c, 1))
+        inet_connect_ports(inet, (d1, 0), (c, 2))
+        # inet.remove_node(u)
+        # wire secondary ports
+        inet_connect_ports(inet, (c0, 1), (d0, 2))
+        inet_connect_ports(inet, (c0, 2), (d1, 2))
+        inet_connect_ports(inet, (c1, 1), (d0, 1))
+        inet_connect_ports(inet, (c1, 2), (d1, 1))
+
+def inet_add_erase(inet: nx.MultiGraph):
     n = inet.number_of_nodes()
     inet.add_node(n, bipartite=1, tag="erase")
     inet.add_node(n+1, bipartite=0)
-    inet.add_edge(n, n+1, index=0)
+    inet.add_edge(n, n+1, key=0)
     return n
 
-def inet_add_construct(inet: nx.Graph):
+def inet_add_construct(inet: nx.MultiGraph):
     n = inet.number_of_nodes()
     inet.add_node(n, bipartite=1, tag="construct")
     inet.add_node(n+1, bipartite=0)
     inet.add_node(n+2, bipartite=0)
     inet.add_node(n+3, bipartite=0)
-    inet.add_edge(n, n+1, index=0)
-    inet.add_edge(n, n+2, index=1)
-    inet.add_edge(n, n+3, index=2)
+    inet.add_edge(n, n+1, key=0)
+    inet.add_edge(n, n+2, key=1)
+    inet.add_edge(n, n+3, key=2)
     return n
 
-def inet_add_duplicate(inet: nx.Graph):
+def inet_add_duplicate(inet: nx.MultiGraph):
     n = inet.number_of_nodes()
     inet.add_node(n, bipartite=1, tag="duplicate")
     inet.add_node(n+1, bipartite=0)
     inet.add_node(n+2, bipartite=0)
     inet.add_node(n+3, bipartite=0)
-    inet.add_edge(n, n+1, index=0)
-    inet.add_edge(n, n+2, index=1)
-    inet.add_edge(n, n+3, index=2)
+    inet.add_edge(n, n+1, key=0)
+    inet.add_edge(n, n+2, key=1)
+    inet.add_edge(n, n+3, key=2)
     return n
 
-def inet_connect_ports(inet: nx.Graph, p0, p1):
+def inet_find_wire(inet: nx.MultiGraph, u, i):
+    for w0, w1, j in inet.edges(u, keys=True):
+        if i == j:
+            return w1 if u == w0 else w0
+    print(inet.nodes(data=True))
+    print(inet.edges(u, keys=True))
+    assert False
+
+def inet_connect_ports(inet: nx.MultiGraph, p0, p1):
     u0, i0 = p0
     u1, i1 = p1
-    old_ports = []
-    for _, b, i in inet.edges(u0, data="index"):
-        if i == i0:
-            old_ports.append(b)
-    for _, b, i in inet.edges(u1, data="index"):
-        if i == i1:
-            old_ports.append(b)
-    inet.remove_nodes_from(old_ports)
-    n = inet.number_of_nodes()
-    inet.add_node(n, bipartite=0)
-    inet.add_edge(n, u0, index=i0)
-    inet.add_edge(n, u1, index=i1)
-    return n
+    w0 = inet_find_wire(inet, u0, i0)
+    # assert len(inet[w0]) == 1
+    w1 = inet_find_wire(inet, u1, i1)
+    # assert len(inet[w1]) == 1
+    # TODO we never remove nodes to ensure increasing IDs
+    inet.remove_edge(u0, w0)
+    inet.remove_edge(u1, w1)
+    w = inet.number_of_nodes()
+    inet.add_node(w, bipartite=0)
+    inet.add_edge(w, u0, key=i0)
+    inet.add_edge(w, u1, key=i1)
+    return w
--- test_inet.py
+++ test_inet.py
@@ -2,26 +2,43 @@ import networkx as nx
 from inet import *
 
 
+
+def test_self_connect_secondary_ports():
+    inet = nx.MultiGraph()
+    c = inet_add_construct(inet)
+    inet_connect_ports(inet, (c, 1), (c, 2))
+    assert len(inet.nodes) == 5
+    assert len(inet.edges) == 3
+    # multiedge
+    assert len(inet[4][0]) == 2
+
+
+def test_connect_ports():
+    inet = nx.MultiGraph()
+    c = inet_add_construct(inet)
+    d = inet_add_duplicate(inet)
+    assert len(inet.nodes) == 8
+    assert len(inet.edges) == 6
+    inet_connect_ports(inet, (c, 0), (d, 0))
+    e = inet_add_erase(inet)
+    assert len(inet.edges) == 7
+    inet_connect_ports(inet, (e, 0), (c, 1))
+    assert len(inet.edges) == 7
+
+
 def test_annihilate_erase_erase():
-    inet = nx.Graph()
+    inet = nx.MultiGraph()
     u = inet_add_erase(inet)
     v = inet_add_erase(inet)
     inet_connect_ports(inet, (u, 0), (v, 0))
     annihilate_erase_erase(inet)
-    assert len(inet) == 0
+    assert len(inet.edges) == 0
 
 
 def test_commute_construct_duplicate():
-    inet = nx.Graph()
+    inet = nx.MultiGraph()
     u = inet_add_construct(inet)
     v = inet_add_duplicate(inet)
     inet_connect_ports(inet, (u, 0), (v, 0))
-    inet.add_node(1, bipartite=0)
-    inet.add_node(2, bipartite=1, tag="construct")
-    inet.add_node(3, bipartite=1, tag="duplicate")
-    inet.add_edge(1, 2, index=0)
-    inet.add_edge(1, 3, index=0)
-
     commute_construct_duplicate(inet)
-    assert len(inet.nodes) == 12
-    assert len(inet.edges) == 0
+    assert len(inet.edges) == 12
