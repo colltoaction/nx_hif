9b2c8d612ba00aa4676cd64d5ea6123df0aaeb9f
Martin Coll <mcoll@dc.uba.ar>
2025-08-08 17:23:12 +0000

    test DPO with inet commute construct-duplicate rule

--- src/nx_hif/dpo.py
+++ src/nx_hif/dpo.py
@@ -9,11 +9,10 @@ https://en.wikipedia.org/wiki/Double_pushout_graph_rewriting
 https://arxiv.org/pdf/2012.01847
 https://www.researchgate.net/publication/220713176_Algebraic_Approaches_to_Graph_Transformation_-_Part_I_Basic_Concepts_and_Double_Pushout_Approach
 """
-import itertools
 import networkx as nx
 
 
-def dpo_rewrite(G: nx.MultiDiGraph, L: nx.MultiDiGraph, R: nx.MultiDiGraph):
+def dpo_rewrite(G: nx.MultiDiGraph, L: nx.MultiDiGraph, R: nx.MultiDiGraph, K: nx.MultiDiGraph):
     """
     To rewrite 𝐺 into 𝐻 we compute the object 𝐶 for the
     pushout complement 𝐾 −→ 𝐶 −→ 𝐺 of 𝐾 −→ 𝐿 −𝑚→ 𝐺.
@@ -21,44 +20,89 @@ def dpo_rewrite(G: nx.MultiDiGraph, L: nx.MultiDiGraph, R: nx.MultiDiGraph):
     so we form the the span 𝐶 ←− 𝐾 −→ 𝑅,
     and obtain 𝐻 as the pushout.
     """
-    # Rule authors will tag their L and R boundary wires.
-    K = dpo_invariant(L, R)
+    # we first relabel to simplify further transformations
+    G = nx.convert_node_labels_to_integers(G)
+    L_sub = nx.MultiDiGraph(L)
+    L_sub.remove_nodes_from(
+        x[0][0] for y, yd in K.nodes(data=True)
+        if yd["bipartite"] == 0
+        for x in K.out_edges(y, keys=True, data=True))
+    R_sub = nx.MultiDiGraph(R)
+    R_sub.remove_nodes_from(
+        x[1][0] for y, yd in K.nodes(data=True)
+        if yd["bipartite"] == 0
+        for x in K.in_edges(y, keys=True, data=True))
 
-    # To find a match we first have to remove the boundary nodes from L.
-    # This is necessary to use isomorphism in cases such as self-loops.
-    L.remove_nodes_from(l for l, d in K.nodes if d["bipartite"] == 0)
     matcher = nx.isomorphism.MultiDiGraphMatcher(
-        G, L,
+        G, L_sub,
         lambda n1, n2:
             n1["bipartite"] == n2["bipartite"] == 0 or
             (n1["bipartite"] == n2["bipartite"] == 1 and n1["tag"] == n2["tag"]),
         lambda _a, _b: True)
 
+    C = nx.MultiDiGraph(G)
+    n = C.number_of_nodes()
+    # shift all R ids by n
+    R_mapping = {r: r+n for r in R_sub.nodes()}
+    R_sub = nx.relabel_nodes(R_sub, R_mapping)
+    C.update(R_sub)
+
+    # for each of those edges, after removing the iso,
+    # we connect C_inner to R_sub.
+    # but the iso doesn't give an ordering.
     for iso in matcher.subgraph_isomorphisms_iter():
-        # each boundary wire has exactly one edge.
-        # the other end is a hyperedge with port that is part of the isomorphic subgraph,
-        # with translation G-->L.
-        # from L
-        for port in K.in_edges:
-            iso[L[port]]
-        # to R
-        for port in K.out_edges:
-            R[iso[port]]
- 
-        # TODO We reattach R removing its boundary,
-        # and replacing each old edge from the boundary wire to a port
-        # with a similar edge from the inner boundary to the port.
-        # when tagging the inner boundary we might have multiple tags for one wire, self loop
-        C = nx.MultiDiGraph(G)
-        C.remove_nodes_from(iso)
-        # We obtain 𝐻 from the pushout of 𝐶 ←− 𝐾 −→ 𝑅.
-        C.add_edges_from(list(R.in_edges(data=True, keys=True)))
-        C.add_edges_from(list(R.out_edges(data=True, keys=True)))
-        C.add_nodes_from(list(R.nodes(data=True)))
-        C.remove_edges_from(list(C.in_edges(keys=True, data=True)))
-        C.remove_edges_from(list(C.out_edges(keys=True, data=True)))
-        return C
-    return G
+        for k1 in K.nodes:
+            if K.nodes[k1]["bipartite"] == 1:
+                wire_boundary_i = k1
+                # a boundary wire has exactly one edge.
+                # the other end is a box with port.
+                # that is part of the isomorphic subgraph,
+                [((left_wire, _), _, _, _)] = K.in_edges(wire_boundary_i, keys=True, data=True)
+                [(_, (right_wire, _), _, _)] = K.out_edges(wire_boundary_i, keys=True, data=True)
+
+                # for each left_wire we have
+                # one edge to the iso subgraph.
+                c_iso_i = None
+                for l_iso, c_iso in iso.items():
+                    if L.has_edge(l_iso, left_wire):
+                        assert not c_iso_i
+                        [(_, _, lk, _)] = L.in_edges(left_wire, keys=True, data=True)
+                        c_iso_i = c_iso
+                    elif L.has_edge(left_wire, l_iso):
+                        assert not c_iso_i
+                        [(_, _, lk, _)] = L.out_edges(left_wire, keys=True, data=True)
+                        c_iso_i = c_iso
+                # From c_iso_i we traverse out to find the inner boundary c_bound_i.
+                # Since c_iso_i might have many edges (even to the same wire, in a loop)
+                # we find the one corresponding to this particular left_wire.
+                # we identify it by the key (port number).
+                c_bound_i = None
+                for a, _, c_iso_i_k, d in C.in_edges(c_iso_i, keys=True, data=True):
+                    if c_iso_i_k == lk:
+                        assert not c_bound_i
+                        c_bound_i = a
+                for _, b, c_iso_i_k, d in C.out_edges(c_iso_i, keys=True, data=True):
+                    if c_iso_i_k == lk:
+                        assert not c_bound_i
+                        c_bound_i = b
+
+                # we reattach c_bound_i to the box in r_sub
+                # the edge key is taken from the right_wire edge in R.
+                # r is the other end of the same-index boundary wire in R
+                if R.in_degree[right_wire] == 1:
+                    [(r_sub_i, _, k, d)] = R.in_edges(right_wire, keys=True, data=True)
+                # shift
+                    r_sub_i += n
+                    C.add_edge(r_sub_i, c_bound_i, k, **d)
+                elif R.out_degree[right_wire] == 1:
+                    [(_, r_sub_i, k, d)] = R.out_edges(right_wire, keys=True, data=True)
+                # shift
+                    r_sub_i += n
+                    C.add_edge(c_bound_i, r_sub_i, k, **d)
+                else:
+                    assert False
+        C.remove_nodes_from(iso.keys())
+    return C
 
 def dpo_invariant(L, R):
     """
@@ -78,34 +122,10 @@ def dpo_invariant(L, R):
     assert len(L_boundary) == len(R_boundary)
 
     for i, (l, r) in enumerate(zip(L_boundary, R_boundary)):
+        K.add_node(i, bipartite=1)
+        K.add_node((l[0], "left"), **l[1])
+        K.add_node((r[0], "right"), **r[1])
+        # TODO add port index as key
         K.add_edge((l[0], "left"), i, key=0)
         K.add_edge(i, (r[0], "right"), key=0)
-    return K
-    
-    # TODO check data for tags
-    for l, d in L.nodes(data=True):
-        if d["bipartite"] == 0:
-            if L.in_degree[l] + L.out_degree[l] == 1:
-                if L.in_degree[l] == 1:
-                    [(y, _, z, de)] = L.in_edges(l, data=True, keys=True)
-                    K.add_node(y, **d)
-                    K.add_edge(y, l, z, **de)
-                if L.out_degree[l] == 1:
-                    [(_, y, z, de)] = L.out_edges(l, data=True, keys=True)
-                    K.add_node(y, **d)
-                    K.add_edge(y, l, z, **de)
-    for r, d in R.nodes(data=True):
-        if d["bipartite"] == 0:
-            if R.in_degree[r] + R.out_degree[r] == 1:
-                if R.in_degree[r] == 1:
-                    [(y, _, z, de)] = R.in_edges(r, data=True, keys=True)
-                    K.add_node(y, **d)
-                    K.add_edge(y, r, z, **de)
-                if R.out_degree[r] == 1:
-                    [(_, y, z, de)] = R.out_edges(r, data=True, keys=True)
-                    K.add_node(y, **d)
-                    K.add_edge(y, r, z, **de)
-    # for k, d in K.nodes(data=True):
-    #     if d["bipartite"] == 0:
-    #         assert K.in_degree[k] == 1 and K.out_degree[k] == 1
     return K
