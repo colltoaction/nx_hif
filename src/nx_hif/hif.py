203ae7b7442864001e7bd1e48260baf2e9eecf4a
Martin Coll <mcoll@dc.uba.ar>
2025-09-08 19:23:25 +0000

    NetworkX temporal demo for issue 2

--- src/nx_hif/hif.py
+++ src/nx_hif/hif.py
@@ -11,54 +11,44 @@ def hif_edges(G: nx.MultiDiGraph, data=False):
         if d["bipartite"] == 1:
             yield (e, d["edge"], d) if data else (e, d["edge"])
 
-def hif_edge_nodes(G: nx.MultiDiGraph, edge=None, data=False):
-    for e0, e1 in hif_edges(G):
-        if edge is None or edge == e1:
-            for e in G.in_edges(e0, keys=True, data=data):
-                ed = G.nodes[e[1]]["edge"]
-                no = G.nodes[e[0]]["node"]
-                yield (ed, no, "tail",) + e[2:]
-            for e in G.out_edges(e0, keys=True, data=data):
-                ed = G.nodes[e[0]]["edge"]
-                no = G.nodes[e[1]]["node"]
-                yield (ed, no, "head",) + e[2:]
-
-def hif_incidences(G: nx.MultiDiGraph, edge=None, node=None, direction=None, key=None, data=False):
-    for e in hif_edge_nodes(G, edge, data=data):
-        if node is None or node == e[1]:
-            if direction is None or direction == e[2]:
-                if key is None or key == e[3]:
-                    yield e
+def hif_incidences(G: nx.MultiDiGraph, data=False):
+    for nx_id_edge, edge in hif_edges(G):
+        for e in G.in_edges(nx_id_edge, keys=True, data=data):
+            nx_id_node = e[0]
+            node = G.nodes[nx_id_node]["node"]
+            yield (nx_id_edge, nx_id_node, edge, node, "tail",) + e[2:]
+        for e in G.out_edges(nx_id_edge, keys=True, data=data):
+            nx_id_node = e[1]
+            node = G.nodes[nx_id_node]["node"]
+            yield (nx_id_edge, nx_id_node, edge, node, "head",) + e[2:]
 
 def hif_add_edge(G, edge, **attr):
-    if not G.has_node(edge):
-        G.add_node(("edge", edge), bipartite=1)
-    attr["edge"] = edge
-    for attr_key, attr_value in attr.items():
-        G.nodes[("edge", edge)][attr_key] = attr_value
-
-def hif_add_node(G, node, **attr):
-    if not G.has_node(node):
-        G.add_node(("node", node), bipartite=0)
-    attr["node"] = node
-    for attr_key, attr_value in attr.items():
-        G.nodes[("node", node)][attr_key] = attr_value
-
-def hif_add_incidence(G: nx.MultiDiGraph, edge, node, direction, key, **attr):
     x = None
-    y = None
     for e0, e1 in hif_edges(G):
         if edge == e1:
             x = e0
-    for n0, n1 in hif_nodes(G):
-        if node == n1:
-            y = n0
     if x is None:
         x = ("edge", edge)
         G.add_node(x, bipartite=1, edge=edge)
+    for attr_key, attr_value in attr.items():
+        G.nodes[x][attr_key] = attr_value
+    return x
+
+def hif_add_node(G, node, **attr):
+    y = None
+    for n0, n1 in hif_nodes(G):
+        if node == n1:
+            y = n0
     if y is None:
         y = ("node", node)
         G.add_node(y, bipartite=0, node=node)
+    for attr_key, attr_value in attr.items():
+        G.nodes[y][attr_key] = attr_value
+    return y
+
+def hif_add_incidence(G: nx.MultiDiGraph, edge, node, direction, key, **attr):
+    x = hif_add_edge(G, edge)
+    y = hif_add_node(G, node)
     if direction == "tail":
         G.add_edge(y, x, key, **attr)
     else:
@@ -71,3 +61,12 @@ def hif_remove_node(G: nx.MultiDiGraph, node):
             e1 = list(G.out_edges(n0))
             G.remove_edges_from(e0)
             G.remove_edges_from(e1)
+
+def hif_remove_incidence(G: nx.MultiDiGraph, node, key):
+    incs = [e
+            for n0, n1 in hif_nodes(G)
+            if node == n1
+            for e in G.out_edges(n0, keys=True)
+            if e[2] == key]
+    print("incs, node, key:", incs, node, key)
+    G.remove_edges_from(incs)
